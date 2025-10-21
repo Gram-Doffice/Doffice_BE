2900b806909d6b2e4447e6c07ae6e9245676c4e4
kusuri12 <waterfail12@naver.com>
2025-10-21 19:37:00 +0900

    공지사항 CRUD 구현 & error 초기 설정
    get 부분 나중에 error 처리 필요

---
+++ src/test/java/gram11/doffice/DofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package gram11.doffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
