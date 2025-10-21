4122bb98652355fe75d2cbb46ef452213f6bac74
kusuri12 <waterfail12@naver.com>
2025-10-21 19:37:00 +0900

    기본 설정

---
+++ src/main/java/gram11/doffice/DofficeApplication.java
@@ -0,0 +1,13 @@
+package gram11.doffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DofficeApplication.class, args);
+	}
+
+}
