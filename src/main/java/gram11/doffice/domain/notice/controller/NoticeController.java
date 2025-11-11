01e5fe072abfb888f8e18499c94235e59f05462b
kusuri12 <waterfail12@naver.com>
2025-11-11 20:06:40 +0900

    예외처리 대충

--- src/main/java/gram11/doffice/domain/notice/controller/NoticeController.java
+++ src/main/java/gram11/doffice/domain/notice/controller/NoticeController.java
@@ -6,10 +6,8 @@ import gram11.doffice.domain.notice.dto.requestDto.UploadImageDto;
 import gram11.doffice.domain.notice.entity.Notice;
 import gram11.doffice.domain.notice.service.NoticeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -23,35 +21,20 @@ public class NoticeController {
     private final NoticeService noticeService;
 
     // 공지글 작성
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(value = "/post", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<String> createNotice(
+    public void createNotice(
             @RequestPart CreateNoticeDto noticeDto,
-            @RequestPart(required = false) UploadImageDto uploadImageDto) {
-        try {
-            noticeService.createNotice(noticeDto, uploadImageDto);
-            return ResponseEntity.status(HttpStatus.CREATED)
-                    .body("공지사항 작성 완료");
-        } catch (IOException e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("이미지 저장 실패");
-        }
+            @RequestPart(required = false) UploadImageDto uploadImageDto)
+            throws IOException {
+        noticeService.createNotice(noticeDto, uploadImageDto);
     }
 
     // 공지글 삭제
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{notice_id}")
-    public ResponseEntity<?> deleteNotice(@PathVariable("notice_id") Long parameter) {
-        try {
-            noticeService.deleteNotice(parameter);
-            return ResponseEntity.noContent().build();
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body(e.getMessage());
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("삭제 중 오류가 발생했습니다.");
-        }
+    public void deleteNotice(@PathVariable("notice_id") Long parameter) {
+        noticeService.deleteNotice(parameter);
     }
 
     // 공지글 상세 조회
@@ -67,19 +50,14 @@ public class NoticeController {
     }
 
     // 공지글 수정
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{notice_id}")
-    public ResponseEntity<String> updateNotice(
+    public void updateNotice(
             @PathVariable("notice_id") Long parameter,
             @RequestPart UpdateNoticeDto noticeDto,
-            @RequestPart(required = false) UploadImageDto uploadImageDto) {
-        try {
-            noticeService.updateNotice(parameter, noticeDto, uploadImageDto);
-            return ResponseEntity.status(HttpStatus.NO_CONTENT)
-                    .body("공지사항 수정 완료");
-        } catch (IOException e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("이미지 저장 실패");
-        }
+            @RequestPart(required = false) UploadImageDto uploadImageDto)
+            throws IOException {
+
+        noticeService.updateNotice(parameter, noticeDto, uploadImageDto);
     }
 }
