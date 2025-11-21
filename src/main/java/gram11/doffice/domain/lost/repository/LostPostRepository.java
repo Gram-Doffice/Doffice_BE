5ba208fe4793d9cc9e620c15ffaa1022a8d5e501
kusuri12 <waterfail12@naver.com>
2025-11-21 15:57:38 +0900

    remove: LostPost 구버전 삭제
    파일명 변경

--- src/main/java/gram11/doffice/domain/lost/repository/LostPostRepository.java
+++
@@ -1,7 +0,0 @@
-package gram11.doffice.domain.lost.repository;
-
-import gram11.doffice.domain.lost.entity.LostPost;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LostPostRepository extends JpaRepository<LostPost, Long> {
-}
