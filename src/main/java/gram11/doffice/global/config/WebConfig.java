32e165881cc4a22530bb9420049af0454f5a003e
kusuri12 <waterfail12@naver.com>
2025-12-23 16:46:49 +0900

    fix: cors 설정 변경

--- src/main/java/gram11/doffice/global/config/WebConfig.java
+++ src/main/java/gram11/doffice/global/config/WebConfig.java
@@ -10,10 +10,12 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "https://gram-doffice.ncloud.sbs",
-                        "http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
+                        "http://localhost:3000",
+                        "http://localhost:5173",
+                        "http://localhost",
+                        "https://gram-doffice.ncloud.sbs"
+                )
+                .allowedMethods("*")
                 .allowCredentials(true);
     }
 }
